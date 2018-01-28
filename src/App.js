cae787e43af8e397b034defe239b884a29861cce
DUNCAN MAK <duncan@aftership.com>
2018-01-28 13:34:23 +0800

    UPT: App.js

--- src/App.js
+++ src/App.js
@@ -27,15 +27,55 @@ class App extends Component {
 
 		const health_care_key = 'Health Care';
 		const medical_key = 'Medical';
-		const tempature_key = 'Tempature';
+		const temperature_key = 'Temperature';
 		const flu_key = 'Flu';
 
 		const data = [
-			{name: '01/01/2018', [line_a_key]: 4000, [line_b_key]: 2400, [health_care_key]: 2400, [medical_key]: 2300, [tempature_key]: 15, [flu_key]: 1000},
-			{name: '01/02/2018', [line_a_key]: 3000, [line_b_key]: 1398, [health_care_key]: 2098, [medical_key]: 1998, [tempature_key]: 7, [flu_key]: 1398},
-			{name: '01/03/2018', [line_a_key]: 2000, [line_b_key]: 9800, [health_care_key]: 7800, [medical_key]: 6800, [tempature_key]: 15, [flu_key]: 900},
-			{name: '01/04/2018', [line_a_key]: 2780, [line_b_key]: 3908, [health_care_key]: 2908, [medical_key]: 2908, [tempature_key]: 20, [flu_key]: 700},
-			{name: '01/05/2018', [line_a_key]: 1890, [line_b_key]: 4800, [health_care_key]: 5400, [medical_key]: 5800, [tempature_key]: 23, [flu_key]: 400},
+			{
+				name: '01/01/2018',
+				[line_a_key]: 4000,
+				[line_b_key]: 2400,
+				[health_care_key]: 2400,
+				[medical_key]: 2300,
+				[temperature_key]: 15,
+				[flu_key]: 1000
+			},
+			{
+				name: '01/02/2018',
+				[line_a_key]: 3000,
+				[line_b_key]: 1398,
+				[health_care_key]: 2098,
+				[medical_key]: 1998,
+				[temperature_key]: 7,
+				[flu_key]: 1398
+			},
+			{
+				name: '01/03/2018',
+				[line_a_key]: 2000,
+				[line_b_key]: 9800,
+				[health_care_key]: 7800,
+				[medical_key]: 6800,
+				[temperature_key]: 15,
+				[flu_key]: 900
+			},
+			{
+				name: '01/04/2018',
+				[line_a_key]: 2780,
+				[line_b_key]: 3908,
+				[health_care_key]: 2908,
+				[medical_key]: 2908,
+				[temperature_key]: 20,
+				[flu_key]: 700
+			},
+			{
+				name: '01/05/2018',
+				[line_a_key]: 1890,
+				[line_b_key]: 4800,
+				[health_care_key]: 5400,
+				[medical_key]: 5800,
+				[temperature_key]: 23,
+				[flu_key]: 400
+			},
 		];
 		return (
 			<div className="App">
@@ -43,23 +83,31 @@ class App extends Component {
 					<img style={{height: 120}} src="http://www.avenuek.com.my/file/2016/06/ws-logo1.jpg"/>
 				</header>
 				<h1 style={{fontSize: 36}} className="App-title">Your Promotion Management Dashboard</h1>
-				<div style={{display: 'flex', 'flex-direction': 'row', 'justify-content': 'space-around', paddingTop: 70}}>
-					<LineChart width={600} height={300} data={data}
-							   margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-						<XAxis dataKey="name">
-						</XAxis>
-						<YAxis>
-							<Label value="Sales (USD)" offset={0} angle={-90} position="insideLeft"/>
-						</YAxis>
-						<CartesianGrid strokeDasharray="3 3"/>
-						<Tooltip/>
-						<Legend/>
-						<Line type="monotone" dataKey={line_a_key} stroke="#8884d8"/>
-						<Line type="monotone" dataKey={line_b_key} stroke="#82ca9d"/>
-					</LineChart>
+				<div style={{
+					display: 'flex',
+					'flex-direction': 'row',
+					'justify-content': 'space-around',
+					paddingTop: 70
+				}}>
+					<div>
+						<h1 style={{fontSize: 24}}>Sales (Medical)</h1>
+						<LineChart width={600} height={300} data={data}
+								   margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+							<XAxis dataKey="name">
+							</XAxis>
+							<YAxis>
+								<Label value="Sales (USD)" offset={0} angle={-90} position="insideLeft"/>
+							</YAxis>
+							<CartesianGrid strokeDasharray="3 3"/>
+							<Tooltip/>
+							<Legend/>
+							<Line type="monotone" dataKey={line_a_key} stroke="#8884d8"/>
+							<Line type="monotone" dataKey={line_b_key} stroke="#82ca9d"/>
+						</LineChart>
+					</div>
 					<div style={{width: 500, margin: 50}}>
 						<div style={{marginTop: 10}}>
-							Percentage of Discount ({this.state.percent}%)
+							<p style={{fontSize: 18}}>Percentage of Discount <b>({this.state.percent}%)</b></p>
 							<SliderWithTooltip onChange={(percent) => {
 								this.setState({
 									percent: percent
@@ -67,7 +115,7 @@ class App extends Component {
 							}} defaultValue={this.state.percent} tipFormatter={percentFormatter}/>
 						</div>
 						<div style={{paddingTop: 30}}>
-							<p>Time Frame</p>
+							<p style={{fontSize: 18}}>Time Frame</p>
 							<div style={{paddingTop: 30}}>
 								From:
 								<DatePicker
@@ -122,7 +170,7 @@ class App extends Component {
 							<CartesianGrid strokeDasharray="3 3"/>
 							<Tooltip/>
 							<Legend/>
-							<Line type="monotone" dataKey={tempature_key} stroke="#8884d8"/>
+							<Line type="monotone" dataKey={temperature_key} stroke="#8884d8"/>
 						</LineChart>
 					</div>
 					<div>
